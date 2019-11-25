faab90b846d2d6279295d38ff4ca9db422cefbc3
Abhijeet Bodhankar <48222006+abbodh@users.noreply.github.com>
2019-11-25 11:20:37 -0800

    modified deployment source file path
    modified deployment source file path to get correct sln and csproj files during deployment.

--- deploy.cmd
+++ deploy.cmd
@@ -72,11 +72,11 @@ SET MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 echo Handling ASP.NET Core Web Application deployment.
 
 :: 1. Restore nuget packages
-call :ExecuteCmd dotnet restore "%DEPLOYMENT_SOURCE%\Source\Microsoft.Teams.Apps.Scrum\Microsoft.Teams.Apps.Scrum.sln"
+call :ExecuteCmd dotnet restore "%DEPLOYMENT_SOURCE%\Microsoft.Teams.Apps.Scrum\Microsoft.Teams.Apps.Scrum.sln"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. Build and publish
-call :ExecuteCmd dotnet publish "%DEPLOYMENT_SOURCE%\Source\Microsoft.Teams.Apps.Scrum\Microsoft.Teams.Apps.Scrum.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
+call :ExecuteCmd dotnet publish "%DEPLOYMENT_SOURCE%\Microsoft.Teams.Apps.Scrum\Microsoft.Teams.Apps.Scrum.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 3. KuduSync
